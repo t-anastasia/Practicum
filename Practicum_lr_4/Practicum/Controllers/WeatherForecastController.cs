ebf4ce722a579320ae38aa877b788573d240baea
t-anastasia <71791307+t-anastasia@users.noreply.github.com>
2021-12-01 18:34:53 +0300

    Add files via upload

---
+++ Practicum_lr_4/Practicum/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practicum.Controllers
+{
+    [ApiController]
+    [Route("test")] //родительский
+
+    public class WeatherForecastController : Controller
+    {
+        //запускается при старте
+        [HttpPost]
+        [HttpGet]
+        [Produces("application/json")] 
+        [Route("index")] //дочерний
+        public async Task<IActionResult> Index()
+        {
+            return this.Json(new { result = "Hello World!" });
+        }
+
+        [HttpPost]
+        [HttpGet]
+        [Produces("application/json")] // возвращаем json
+        [Route("hello_world")]
+        public async Task<IActionResult> Test()
+        {
+            MyClassResponse res = new MyClassResponse();
+            res.Success = "success";
+            res.Result = "hello_world";
+            res.Version = "1.1";
+            return this.Json(res);
+        }
+    }
+}
